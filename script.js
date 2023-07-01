c4f95506f7c5f5221cb19f4863e2a70a00e1613e
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-07-01 14:16:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,31 +6,31 @@ window.promises = [];
 let promise1 = new Promise((resolve, reject) => {
     setTimeout(() =>{
         resolve("Promise1");
-    }, Math.floor(Math.random() * 5000) + 1000);
+    }, Math.floor(Math.random() * 5));
 });
 
 let promise2 = new Promise((resolve, reject) => {
     setTimeout(() =>{
         resolve("Promise2");
-    }, Math.floor(Math.random() * 5000) + 1000);
+    }, Math.floor(Math.random() * 5));
 });
 
 let promise3 = new Promise((resolve, reject) => {
     setTimeout(() =>{
         resolve("Promise3");
-    }, Math.floor(Math.random() * 5000) + 1000);
+    }, Math.floor(Math.random() * 5));
 });
 
 let promise4 = new Promise((resolve, reject) => {
     setTimeout(() =>{
         resolve("Promise4");
-    }, Math.floor(Math.random() * 5000) + 1000);
+    }, Math.floor(Math.random() * 5));
 });
 
 let promise5 = new Promise((resolve, reject) => {
     setTimeout(() =>{
         resolve("Promise5");
-    }, Math.floor(Math.random() * 5000) + 1000);
+    }, Math.floor(Math.random() * 5));
 });
 
 const myArray = [promise1, promise2, promise3, promise4, promise5];
