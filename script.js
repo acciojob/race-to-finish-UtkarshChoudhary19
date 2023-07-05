91531342228c326f484029d225c7420d19244314
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-07-05 17:51:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,33 +4,38 @@ window.promises = [];
 // add your promises to the array `promises`
 
 let promise1 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise1");
-    }, Math.floor(Math.random() * 5));
+    let randomTime = Math.floor(Math.random() * 5) + 1;
+    setTimeout(() => {
+        resolve(randomTime);
+    }, randomTime * 1000);
 });
 
 let promise2 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise2");
-    }, Math.floor(Math.random() * 5));
+    let randomTime = Math.floor(Math.random() * 5) + 1;
+    setTimeout(() => {
+        resolve(randomTime);
+    }, randomTime * 1000);
 });
 
 let promise3 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise3");
-    }, Math.floor(Math.random() * 5));
+    let randomTime = Math.floor(Math.random() * 5) + 1;
+    setTimeout(() => {
+        resolve(randomTime);
+    }, randomTime * 1000);
 });
 
 let promise4 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise4");
-    }, Math.floor(Math.random() * 5));
+    let randomTime = Math.floor(Math.random() * 5) + 1;
+    setTimeout(() => {
+        resolve(randomTime);
+    }, randomTime * 1000);
 });
 
 let promise5 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise5");
-    }, Math.floor(Math.random() * 5));
+    let randomTime = Math.floor(Math.random() * 5) + 1;
+    setTimeout(() => {
+        resolve(randomTime);
+    }, randomTime * 1000);
 });
 
 const promises = [promise1, promise2, promise3, promise4, promise5];
